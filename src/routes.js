bde42ae01caa243c0643bb7267f252b957162787
Isaac Oduh <ufuomaoduh@gmail.com>
2021-05-09 00:32:47 +0100

    Category Frontend

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,11 @@ let storeemployee = require("./components/employee/create.vue").default;
 let employee = require("./components/employee/index.vue").default;
 let editemployee = require("./components/employee/edit.vue").default;
 
+// Category Component
+let storecategory = require("./components/category/create.vue").default;
+let category = require("./components/category/index.vue").default;
+let editcategory = require("./components/category/edit.vue").default;
+
 export const routes = [
     { path: "/", component: login, name: "/" },
     { path: "/register", component: register, name: "register" },
@@ -26,5 +31,14 @@ export const routes = [
         path: "/edit-employee/:id",
         component: editemployee,
         name: "edit-employee",
+    },
+
+    // Category Routes
+    { path: "/store-category", component: storecategory, name: "store-category" },
+    { path: "/category", component: category, name: "category" },
+    {
+        path: "/edit-category/:id",
+        component: editcategory,
+        name: "edit-category",
     },
 ];
